808a8d924df9e30285192b767426067ba1aec157
Alireza <a.mahdavi@tamu.edu>
2023-10-18 15:06:20 -0500

    interface complete

--- phase_2/Ali_phase_3/newItemsGUI.java
+++ phase_2/Ali_phase_3/newItemsGUI.java
@@ -1,13 +1,12 @@
 import java.sql.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.*;
-import java.awt.GridLayout;
-import java.awt.FlowLayout;
-import java.awt.Dimension;
+import java.util.List; // Specify java.util.List
+import java.util.ArrayList; // Specify java.util.ArrayList
+import java.awt.*;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.BorderLayout; // Import BorderLayout from java.awt
+import java.awt.BorderLayout;
 
 public class newItemsGUI extends JFrame implements ActionListener {
     static JFrame f;
@@ -15,11 +14,21 @@ public class newItemsGUI extends JFrame implements ActionListener {
     public static JButton addSub;
     public static JButton returnButton;
     public static JTextField itemNameField;
+    public static JTextField priceField; // Added input field for price
+    public static JRadioButton dairyRadio;
+    public static JRadioButton notDairyRadio;
+    public static ButtonGroup dairyButtonGroup;
 
     static Connection conn = null;
 
     public newItemsGUI() {
         itemNameField = new JTextField(20);
+        priceField = new JTextField(20); // Initialize the price field
+        dairyRadio = new JRadioButton("Dairy");
+        notDairyRadio = new JRadioButton("Not Dairy");
+        dairyButtonGroup = new ButtonGroup();
+        dairyButtonGroup.add(dairyRadio);
+        dairyButtonGroup.add(notDairyRadio);
     }
 
     public static void itemGUI() {
@@ -72,25 +81,29 @@ public class newItemsGUI extends JFrame implements ActionListener {
             pButtons.add(button);
         }
 
-        // Create a panel for input fields
+        // Create a panel for input fields, including item name, price, and radio buttons
         JPanel pInput = new JPanel();
-        pInput.setLayout(new BoxLayout(pInput, BoxLayout.LINE_AXIS));
-        pInput.setPreferredSize(new Dimension(400, 15));
+        pInput.setLayout(new BoxLayout(pInput, BoxLayout.LINE_AXIS)); // Horizontal layout
+        pInput.setBackground(new Color(0xCC601D));
         pInput.add(new JLabel("Item Name: "));
         pInput.add(itemNameField);
+        pInput.add(new JLabel("Price: "));
+        pInput.add(priceField);
+        pInput.add(new JLabel("Dairy:"));
+        pInput.add(dairyRadio);
+        pInput.add(notDairyRadio);
 
         addSub = new JButton("Add");
-        addSub.setPreferredSize(new Dimension(400, 30));
+        addSub.setPreferredSize(new Dimension(200, 50));
         addSub.setBackground(new Color(0x77DD77));
         addSub.setFont(buttonFont);
         addSub.addActionListener(s);
 
         // Move the "Return" button to the left of the "Add" button
         returnButton = new JButton("Return");
-        returnButton.setPreferredSize(new Dimension(400, 30));
+        returnButton.setPreferredSize(new Dimension(200, 50));
         returnButton.setBackground(new Color(0xFF6961));
         returnButton.addActionListener(s);
-
 
         pButtons.add(returnButton);
         pButtons.add(addSub);
@@ -99,8 +112,7 @@ public class newItemsGUI extends JFrame implements ActionListener {
         JPanel pMain = new JPanel(new BorderLayout());
         pMain.setBackground(new Color(0xCC601D));
         pMain.add(pButtons, BorderLayout.CENTER);
-        pMain.add(pInput, BorderLayout.NORTH);
-
+        pMain.add(pInput, BorderLayout.NORTH); // Position input fields at the top
 
         // Add the main panel to the frame
         f.add(pMain);
@@ -118,10 +130,14 @@ public class newItemsGUI extends JFrame implements ActionListener {
         if (s.equals("Return")) {
             f.dispose();
         } else if (s.equals("Add")) {
-            // Get the item name from the input field
+            // Get the item name and price from the input fields
             String itemName = itemNameField.getText();
+            String price = priceField.getText();
 
-            // You can now use itemName and the selected ingredients to add the new item to your database.
+            // Check the selected radio button to determine if it's dairy or not
+            boolean isDairy = dairyRadio.isSelected();
+
+            // You can now use itemName, price, isDairy, and the selected ingredients to add the new item to your database.
             // Add the necessary database logic here.
         } else {
             // Handle ingredient selection here.
